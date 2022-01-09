d297d76571dea365689da461e9942ec8182a4a12
Pablo Vannini <vanninipablo@gmail.com>
2022-01-09 17:22:57 +0000

    implement other functions

--- src/main/java/com/gmail/teampagnus/exposed_ores/MoonCraterPopulator.java
+++ src/main/java/com/gmail/teampagnus/exposed_ores/MoonCraterPopulator.java
@@ -2,26 +2,11 @@ package com.gmail.teampagnus.exposed_ores;
 
 import java.util.Random;
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.generator.BlockPopulator;
 
 public class MoonCraterPopulator extends BlockPopulator {
     public void populate(World world, Random random, Chunk source) {
-        for (int x = 0; x < 16; x++)
-        {
-            for (int y = 0; y < 200; y++)
-            {
-                for (int z = 0; z < 16; z++)
-                {
-                    Block b = source.getBlock(x, y, z);
-                    if (b.getType()  != Material.AIR)
-                    {
-                        b.setType(Material.TNT, false);
-                    }
-                }
-            }
-        }
+        OreRemover.mainOnlyOres(world, random, source);
     }
 }
--- src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
+++ src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
@@ -1,9 +1,12 @@
 package com.gmail.teampagnus.exposed_ores;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
 class DesiredBlock {
@@ -29,6 +32,59 @@ public class OreRemover {
 				}
 			}
 		}
+	}
+
+	public static Boolean oreHasExposedNeighbor(DesiredBlock db, Chunk ch){
+		return true;
+	}
+
+	public static Boolean updateExposedOres(ArrayList<DesiredBlock> desiredBlockList, Chunk chunk){
+		Boolean someChange = false;
+		for (int i = 0; i < desiredBlockList.size(); i++){
+			DesiredBlock db = desiredBlockList.get(i);
+			someChange = someChange || oreHasExposedNeighbor(db, chunk);
+		}
+		return someChange;
+	}
+
+	public static void oreRemoverReplacement(ArrayList<DesiredBlock> desiredBlockList, Material dest){
+		for (int i = 0; i < desiredBlockList.size(); i++){
+			DesiredBlock db = desiredBlockList.get(i);
+			if (!db.isDesired){
+				db.block.setType(dest, false);
+			}
+		}
+	}
+
+	private static int MAX_UPDATES = 100;
+	private static ArrayList<Material> materialList = new ArrayList<Material>(Arrays.asList(Material.REDSTONE_ORE, Material.COAL_ORE, Material.GOLD_ORE, Material.IRON_ORE, Material.DIAMOND_ORE));
+
+	public static void main(World world, Random random, Chunk source){
+		ArrayList<DesiredBlock> desiredBlockList = new ArrayList<DesiredBlock>();
+		OreRemover.getOresInChunk(desiredBlockList, source, materialList);
+		for (int i = 0; i < MAX_UPDATES; i++){
+			if (OreRemover.updateExposedOres(desiredBlockList, source)){
+				i = MAX_UPDATES;
+			}
+		}
+		OreRemover.oreRemoverReplacement(desiredBlockList, Material.STONE);
+	}
+
+	public static void mainOnlyOres(World world, Random random, Chunk source){
+		int maxHeight = source.getWorld().getMaxHeight();
+		int minHeight = source.getWorld().getMinHeight();
+
+        for (int x = 0; x < 16; x++) {
+            for (int y = minHeight; y < maxHeight; y++) {
+                for (int z = 0; z < 16; z++) {
+                    Block b = source.getBlock(x, y, z);
+                    if (!b.getType().name().endsWith("_ORE"))
+                    {
+                        b.setType(Material.AIR, false);
+                    }
+                }
+            }
+        }
 	}
 
 	public static void replace(Chunk chunk) {
