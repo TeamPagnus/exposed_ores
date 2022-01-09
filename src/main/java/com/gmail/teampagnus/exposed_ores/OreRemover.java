5e8884697168b682df9e52527d4cb9503663e204
absentabyss <agna.lumi@protonmail.com>
2022-01-09 01:32:15 -0300

    Passed the TNT test

--- src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
+++ src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
@@ -1,13 +1,22 @@
 package com.gmail.teampagnus.exposed_ores;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkPopulateEvent;
 
 public final class OreRemover implements Listener {
-    @EventHandler()
-    public void onChunkPopulate(ChunkPopulateEvent event) {
-    	event.getChunk().getBlock(0, 64, 0).setType(Material.DIAMOND_BLOCK, false);
-    }
+	@EventHandler()
+	public void onChunkPopulate(ChunkPopulateEvent event) {
+		for (int X = 0; X < 16; X++) {
+			for (int Z = 0; Z < 16; Z++) {
+				for (int Y = 256; Y > 0; Y--) {
+					Block block = event.getChunk().getBlock(X, Y, Z);
+					if (!block.getType().equals(Material.AIR))
+						event.getChunk().getBlock(X, Y, Z).setType(Material.TNT, false);
+				}
+			}
+		}
+	}
 }
