37ade62dbc1a26f23b826ee6db452eb7249e7b5c
absentabyss <agna.lumi@protonmail.com>
2022-01-09 14:18:59 -0300

    Implemented oreHasExposedNeighbor

--- src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
+++ src/main/java/com/gmail/teampagnus/exposed_ores/OreRemover.java
@@ -3,13 +3,14 @@ package com.gmail.teampagnus.exposed_ores;
 import java.util.ArrayList;
 
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 class DesiredBlock {
 	public Block block;
 	public Boolean isDesired;
-	
+
 	public DesiredBlock(Block block, Boolean isDesired) {
 		this.block = block;
 		this.isDesired = isDesired;
@@ -17,18 +18,42 @@ class DesiredBlock {
 }
 
 public class OreRemover {
-	public static void getOresInChunk(ArrayList<DesiredBlock> desiredBlockList, Chunk chunk, ArrayList<Material> materialList) {
+	private void getOresInChunk(ArrayList<DesiredBlock> desiredBlockList, Chunk chunk,
+			ArrayList<Material> materialList) {
 		int maxHeight = chunk.getWorld().getMaxHeight();
 		int minHeight = chunk.getWorld().getMinHeight();
 		for (int x = 0; x < 16; x++) {
 			for (int z = 0; z < 16; z++) {
 				for (int y = minHeight; y < maxHeight; y++) {
 					Block block = chunk.getBlock(x, y, z);
-					if (materialList.contains(block.getType()));
-						desiredBlockList.add(new DesiredBlock(block, false));
+					if (materialList.contains(block.getType()))
+						;
+					desiredBlockList.add(new DesiredBlock(block, false));
 				}
 			}
 		}
+	}
+
+	private Boolean oreHasExposedNeighbor(DesiredBlock block, Chunk chunk, ArrayList<DesiredBlock> desiredBlockList,
+			ArrayList<Material> materialList) {
+		int a[] = new int[2];
+		a[0] = -1;
+		a[1] = 1;
+		for (int side : a) {
+			Location location = block.block.getLocation();
+			Block block2 = chunk.getBlock((int) (location.getX() + side), (int) (location.getY() + side),
+					(int) (location.getZ() + side));
+			if (block2.getType().equals(Material.AIR))
+				return true;
+			if (materialList.contains(block2.getType())) {
+				for (DesiredBlock ds : desiredBlockList) {
+					if (ds.block.equals(chunk.getBlock((int) (location.getX() + side), (int) (location.getY() + side),
+							(int) (location.getZ() + side))) && ds.isDesired)
+						return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void replace(Chunk chunk) {
