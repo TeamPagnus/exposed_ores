77cfeca22d8ae75848cd5881d57ef1eb54eec99c
Pablo Vannini <vanninipablo@gmail.com>
2022-01-10 01:40:35 +0000

    remove useless imports

--- src/main/java/com/gmail/teampagnus/exposed_ores/Exposed_ores.java
+++ src/main/java/com/gmail/teampagnus/exposed_ores/Exposed_ores.java
@@ -1,14 +1,6 @@
 package com.gmail.teampagnus.exposed_ores;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.world.WorldInitEvent;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.World;
 
 public final class Exposed_ores extends JavaPlugin {
     @Override
